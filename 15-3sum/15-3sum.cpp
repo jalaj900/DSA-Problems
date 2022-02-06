b2b804a310e633c7a951eb128c0bd489073ee975
Jalaj Sahai <67186159+jalaj900@users.noreply.github.com>
2022-02-06 16:18:54 +0530

    Time: 105 ms (62.59%), Space: 21.2 MB (38.43%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+            
+    vector<vector<int> > res;
+
+    std::sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < nums.size(); i++) {
+        
+        int target = -nums[i];
+        int front = i + 1;
+        int back = nums.size() - 1;
+
+        while (front < back) {
+
+            int sum = nums[front] + nums[back];
+            
+            // Finding answer which start from number num[i]
+            if (sum < target)
+                front++;
+
+            else if (sum > target)
+                back--;
+
+            else {
+                vector<int> triplet = {nums[i], nums[front], nums[back]};
+                res.push_back(triplet);
+                
+                // Processing duplicates of Number 2
+                // Rolling the front pointer to the next different number forwards
+                while (front < back && nums[front] == triplet[1]) front++;
+
+                // Processing duplicates of Number 3
+                // Rolling the back pointer to the next different number backwards
+                while (front < back && nums[back] == triplet[2]) back--;
+            }
+            
+        }
+
+        // Processing duplicates of Number 1
+        while (i + 1 < nums.size() && nums[i + 1] == nums[i]) 
+            i++;
+
+    }
+    
+    return res;
+    
+        
+    }
+};
