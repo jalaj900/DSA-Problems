03beff9ca707bc4b1205e0b9551c85e0271176b6
Jalaj Sahai <67186159+jalaj900@users.noreply.github.com>
2022-02-06 16:18:32 +0530

    Time: 16 ms (89.55%), Space: 19.5 MB (99.39%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+            
+            int n=nums.size();
+            int count =0;
+            int ele=0;
+            
+            for(int i=0;i<n;i++)
+            {
+                    if (count==0)
+                            ele=nums[i];
+                    
+                    if(ele==nums[i])
+                            count ++;
+                    else
+                            count--;
+                    
+            }
+            
+            return ele;
+    }
+};
