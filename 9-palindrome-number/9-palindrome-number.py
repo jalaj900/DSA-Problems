c64b0e6ccdc2ddf0f7363d354360c5a51cb329df
Jalaj Sahai <67186159+jalaj900@users.noreply.github.com>
2022-03-10 15:16:43 +0530

    Time: 117 ms (18.82%), Space: 13.6 MB (13.78%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        y=str(x)
+        
+        if y==y[::-1]:
+                return True
+        else:
+                return False
+        
